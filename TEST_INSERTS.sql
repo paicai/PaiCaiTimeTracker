b79a01cb7cad89b008a44552889e351fe7e05d96
Gregor <gmavec@gmail.com>
2018-04-25 16:15:14 +0200

    Update TEST_INSERTS.sql

--- TEST_INSERTS.sql
+++ TEST_INSERTS.sql
@@ -2,7 +2,7 @@ insert into users (username, password, first_name, last_name)
 values ('gregor.mavec@c-automobil-import.si','secure','Gregor','Mavec');
 
 insert into CHECK_IN (check_in_time, check_in_type, user_id) 
-values (current_timestamp, 'P', 1);
+values (current_timestamp, 'A', 1);
 
 update CHECK_IN 
 set check_in_type = 'O'
